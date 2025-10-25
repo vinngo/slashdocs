2354d703d32c396373168229065989f533e0eea9
vinngo <ngovincent9593@gmail.com>
2025-10-25 03:16:33 -0700

    add: docs navbar

---
+++ frontend/src/components/docs/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="z-20 bg-background border-b w-full">
+      <div className="flex h-16 items-center justify-between px-6">
+        <span className="text-xl font-bold flex-shrink-0">/docs</span>
+        <span>microsoft/vscode</span>
+        <span>Share</span>
+      </div>
+    </div>
+  );
+}
